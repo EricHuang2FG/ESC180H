a6081a1a3b7c5c5bc885b522c96e542df9910a16
Eric Huang <xilahuade@foxmail.com>
2024-09-13 17:24:19 -0400

    Added a file that would ring bells

---
+++ ring_bell.py
@@ -0,0 +1,10 @@
+import time, random
+
+def ring_bell():
+    for i in range(10000):
+        print("\a")
+        t = random.randint(1, 30)
+        time.sleep(t / 100)
+
+if __name__ == "__main__":
+    ring_bell()

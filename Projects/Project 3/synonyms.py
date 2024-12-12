4b6983cbe81e43b5cbf35d293084d222974b1b7d
Eric Huang <xilahuade@foxmail.com>
2024-12-11 23:32:57 -0500

    Lots of stuff incoming

--- Projects/Project 3/synonyms.py
+++ Projects/Project 3/synonyms.py
@@ -17,46 +17,46 @@ def build_semantic_descriptors(sentences) -> dict:
     semantic_descriptors = {}
     for sentence in sentences:
         sentence = list(set(sentence))
+        sentence = [i for i in sentence if i]
         for word_key in sentence:
+            word_key = word_key.lower()
             if not word_key: 
                 continue
             if word_key not in semantic_descriptors:
                 semantic_descriptors[word_key] = {}
             for neighbour_word in sentence:
+                neighbour_word = neighbour_word.lower()
                 if neighbour_word != word_key and neighbour_word:
                     if neighbour_word not in semantic_descriptors[word_key]:
                         semantic_descriptors[word_key][neighbour_word] = 1
                     else:
                         semantic_descriptors[word_key][neighbour_word] += 1
+    semantic_descriptors = {key: value for key, value in semantic_descriptors.items() if value}
     return semantic_descriptors
 
 def build_semantic_descriptors_from_files(filenames) -> dict:
     all_text = ""
     for file in filenames:
-        with open(file, "r", encoding="latin1") as f:
-            all_text += f.read()
-    for punc in [",", "-", "--", ":", ";", "'", "(", ")"]:
-        all_text = all_text.replace(punc, "")
+        with open(file, "r", encoding="utf-8") as f:
+            all_text += f.read() + ". "
+    for punc in [",", "-", "--", ":", ";"]:
+        all_text = all_text.replace(punc, " ")
     all_text = re.sub(r"\n+", " ", all_text)
-    all_text = all_text.replace("?", ".").replace("!", ".").split(".")
+    all_text = all_text.replace("?", ".").replace("!", ".").replace("(", "").replace(")", "").split(".")
     all_text = [[w.lower().strip() for w in sentence.split(" ")] for sentence in all_text]
     return build_semantic_descriptors(all_text)
 
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
-    most_similar_words, most_similar_similarity = [], -2
+    most_similar_word, most_similar_similarity = "", -2
     for choice in choices:
         if choice in semantic_descriptors and word in semantic_descriptors:
             similarity = similarity_fn(semantic_descriptors[word], semantic_descriptors[choice])
         else:
             similarity = -1
         if similarity > most_similar_similarity:
-            most_similar_words.append({choice: similarity})
+            most_similar_word = choice
             most_similar_similarity = similarity
-    for pair in most_similar_words:
-        word = list(pair.keys())[0]
-        count = pair[word]
-        if count == most_similar_similarity:
-            return word # this return is guaranteed since most_similar_words is guaranteed to be non-empty
+    return most_similar_word
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     total, correct = 0, 0
@@ -70,20 +70,7 @@ def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     return (correct / total) * 100
 
 if __name__ == "__main__":
-    # vec2 = {"i": 3, "am": 3, "a": 2, "sick": 1, "spiteful": 1, "an": 1, "unattractive": 1}
-    # vec1 = {"i": 1, "believe": 1, "my": 1, "is": 1, "diseased": 1}
-    # print(cosine_similarity(vec1, vec2))
-    # sentences = [["i", "am", "a", "sick", "man"],
-    # ["i", "am", "a", "spiteful", "man"],
-    # ["i", "am", "an", "unattractive", "man"],
-    # ["i", "believe", "my", "liver", "is", "diseased"],
-    # ["however", "i", "know", "nothing", "at", "all", "about", "my",
-    # "disease", "and", "do", "not", "know", "for", "certain", "what", "ails", "me"]]
-    # print(build_semantic_descriptors(sentences)['man'])
-    # vecs = {'a1':{'a2':1,'a3':2},'a4':{'a2':2,'a3':1},'a5':{'a2':4,'a3':4}}
-    # choices = ['blah','a4','a5']
-    # word = 'a1'
-    # print(most_similar_word(word, choices, vecs, cheb_sim))
-    filename = "test.txt"
-    semantic_descriptors = build_semantic_descriptors_from_files(["war_and_peace.txt", "swanns_way.txt"])
-    print(run_similarity_test(filename, semantic_descriptors, cosine_similarity))
+    # filename = "test.txt"
+    # semantic_descriptors = build_semantic_descriptors_from_files(["war_and_peace.txt", "swanns_way.txt"])
+    # print(run_similarity_test(filename, semantic_descriptors, cosine_similarity))
+    pass
--- Projects/Project 3/synonyms_compare.py
+++ Projects/Project 3/synonyms_compare.py
@@ -1,23 +1,71 @@
 import math
-
-def norm(vec):
-    sum_of_squares = 0.0  
-    for x in vec:
-        sum_of_squares += vec[x] * vec[x]
-    
-    return math.sqrt(sum_of_squares)
+import re
+import time
 
 def cosine_similarity(vec1, vec2):
-    pass
+    dot = sum(vec1[key] * vec2[key] for key in vec1 if key in vec2)
+    return dot / math.sqrt(sum(value ** 2 for value in vec1.values()) * sum(value ** 2 for value in vec2.values()))
 
 def build_semantic_descriptors(sentences):
-    pass
+    d = {}
+    for sentence in sentences:
+        unique_words = {}
+        for word in sentence:
+            word_lower = word.lower()
+            unique_words[word_lower] = 1
+        for word in unique_words:
+            if word not in d:
+                d[word] = {}
+            for other_word in unique_words:
+                if word != other_word:
+                    if other_word in d[word]:
+                        d[word][other_word] += 1
+                    else:
+                        d[word][other_word] = 1
+    return d
 
 def build_semantic_descriptors_from_files(filenames):
-    pass
-
+    s = ""
+    for file in filenames:
+        with open(file, "r", encoding="utf-8") as f:
+            s += f.read() + "."
+    s = re.sub(r'[.!?]', '|', s.replace('\n',' ')).split('|')
+    for sentence in range(len(s)):
+        s[sentence] = re.sub(r'[^a-zA-Z\s]', '', s[sentence]).split()
+    # return build_semantic_descriptors(s)
+    hi = build_semantic_descriptors(s)
+    return hi
+# IF WORD IS NOT IN SEMANTIC DESCRIPTORS, WHAT DO I DO???? RETURN NONE OR FIRST CHOICE?
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
-    pass
+    max_score, max_word = -2, ""
+    a = semantic_descriptors.get(word, None)
+    for choice in choices:
+        b = semantic_descriptors.get(choice, None)
+        score = similarity_fn(a, b) if a and b else -1
+        if score > max_score:
+            max_score = score
+            max_word = choice
+    return max_word
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
-    pass
+    questions = open(filename, "r", encoding="utf-8").readlines()
+    for i in range(len(questions)):
+        questions[i] = questions[i].split()
+    count = 0
+    for question in questions:
+        count += most_similar_word(question[0], question[2:], semantic_descriptors, similarity_fn) == question[1]
+    return 100 * count / len(questions)
+
+def norm(vec):
+    sum_of_squares = 0.0
+    for x in vec:
+        sum_of_squares += vec[x] * vec[x]
+    return math.sqrt(sum_of_squares)
+
+# start = time.time()
+# sem = build_semantic_descriptors_from_files(["aayush_smart_train.txt"])
+# print(f"sem = {sem}")
+# print(f"time taken: {time.time() - start}")
+#
+# score = run_similarity_test("input.txt", sem, cosine_similarity)
+# print(score)
